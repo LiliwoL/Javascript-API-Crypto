e8d45a6ca4528fa8da9709f90164c21e4ec2e3c9
Nicolas C. <github@liliwol.fr>
2024-07-24 23:46:45 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Javascript API for CoinGecko
+
+![](readme_docs/77ef86d0.png)
+
+Apprendre à utiliser une API externe en Javascript.
+
+# Prérequis
+
+Disposer d'une clé API sur coingecko.com
+
+# Configuration
+
+Créer un fichier `env.js` dans le dossier **assets/js** et ajouter la ligne suivante:
+
+```env
+const API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+```
+
+---
+
+# API documentation
+
+https://docs.coingecko.com/v3.0.1/reference/introduction
+
+## Authentification
+
+https://docs.coingecko.com/v3.0.1/reference/authentication
+
+## Endpoints
+
+https://docs.coingecko.com/v3.0.1/reference/endpoint-overview
---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+
+const API_ENDPOINT = "https://api.coingecko.com/api/v3/";
+
+const API_AUTH_SUFFIX = "x_cg_demo_api_key=" + API_KEY;
+
+// https://docs.coingecko.com/v3.0.1/reference/endpoint-overview
+const API_URLS = {
+    // Liste des coins
+    'coins': API_ENDPOINT + 'coins/list' + '?' + API_AUTH_SUFFIX,
+    'price' : API_ENDPOINT + 'simple/price?ids=ethereum&vs_currencies=eur&' + API_AUTH_SUFFIX,
+}
+
+/**
+ * Liste des cryptos disponibles
+ */
+function callAPI( endpoint ) {
+    fetch(endpoint)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log("Erreur de récupération API " + error);
+        }
+    );
+}
+
+// Appel de la fonction getCoins
+console.log("Liste");
+callAPI(API_URLS.coins);
+
+console.log("Prix Ethereum");
+callAPI(API_URLS.price);
